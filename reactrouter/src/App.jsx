4f405aa1505fb40e4d87baf75a699bd7738cfc4d
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-05-11 16:45:25 -0300

    Importar pages

--- reactrouter/src/App.jsx
+++ reactrouter/src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
     <div className="App">
