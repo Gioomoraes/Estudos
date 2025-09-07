67e2cf7b6ea92eac632f7de4ac5e07657a925615
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-09-07 10:54:33 -0300

    feat: add name display and re-rendering with useState

--- src/components/HookUseState.jsx
+++ src/components/HookUseState.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from "react";
 
 const HookUseState = () => {
+  let userName = "Giovanna";
+
+  const [name, setName] = useState("linda");
+
+  const changeNames = () => {
+    userName = "Giovanna Moraes";
+
+    setName("Maravilhosa");
+
+    console.log(userName);
+  };
+  console.log(name);
+
   return (
     <div>
-      
+      <h2>Hook useState</h2>
+      <p>Variável: {userName}</p>
+      <p>useState: {name}</p>
+      <button onClick={changeNames}>Mudar Nomes</button>
     </div>
-  )
-}
+  );
+};
 
-export default HookUseState
+export default HookUseState;
