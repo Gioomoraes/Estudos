d33db4fe0078da98099f62bdd9e809adb5cedd73
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-09-28 14:17:14 -0300

    feat: add age input field with state and display

--- src/components/HookUseState.jsx
+++ src/components/HookUseState.jsx
@@ -14,12 +14,25 @@ const HookUseState = () => {
   };
   console.log(name);
 
+  const [age, setAge] = useState(18);
+
   return (
     <div>
       <h2>Hook useState</h2>
       <p>Variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mudar Nomes</button>
+
+      <form>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </form>
+      <p>Sua idade é: {age}</p>
+
+      <hr />
     </div>
   );
 };
