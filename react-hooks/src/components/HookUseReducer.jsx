9a02a1ab199b3c54000421034749e7bcc9a41d77
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-12-11 08:18:24 -0300

    feat: implement basic HookUseReducer component with random number state

--- react-hooks/src/components/HookUseReducer.jsx
+++ react-hooks/src/components/HookUseReducer.jsx
@@ -1,7 +1,17 @@
 import { useReducer } from "react";
 
 const HookUseReducer = () => {
-  return <div></div>;
+  const [number, dispatch] = useReducer((state, action) => {
+    return Math.random(state);
+  });
+  return (
+    <div>
+      <h2>Hook useReducer</h2>
+      <p>Número: {number}</p>
+      <button onClick={dispatch}>Alterar o número!</button>
+      <hr />
+    </div>
+  );
 };
 
 export default HookUseReducer;
--- react-hooks/src/pages/Home.jsx
+++ react-hooks/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HookUseState from "../components/HookUseState";
+import HookUseReducer from "../components/HookUseReducer";
 
 const Home = () => {
   return (
