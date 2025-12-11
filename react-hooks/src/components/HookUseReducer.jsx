1446e841eedf7ef4c8c02ad49862c4f7206cf5e3
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-12-11 08:02:20 -0300

    feat: import useReducer and add HookUseReducer component file.

---
+++ react-hooks/src/components/HookUseReducer.jsx
@@ -0,0 +1,7 @@
+import { useReducer } from "react";
+
+const HookUseReducer = () => {
+  return <div></div>;
+};
+
+export default HookUseReducer;
--- react-hooks/src/pages/Home.jsx
+++ react-hooks/src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div>
       <HookUseState />
+      <HookUseReducer />
     </div>
   );
 };
