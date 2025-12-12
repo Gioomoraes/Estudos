a783d39e2820b59bd8c5e46160ee4eb2b018d69a
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-12-12 09:08:21 -0300

    feat: add task creation and double-click deletion using useReducer

--- react-hooks/src/components/HookUseReducer.jsx
+++ react-hooks/src/components/HookUseReducer.jsx
@@ -1,14 +1,62 @@
-import { useReducer } from "react";
+import { useState, useReducer } from "react";
 
 const HookUseReducer = () => {
   const [number, dispatch] = useReducer((state, action) => {
     return Math.random(state);
   });
+
+  const initialTasks = [
+    { id: 1, text: "Fazer alguma coisa" },
+    { id: 2, text: "Fazer outra coisa" },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(),
+          text: taskText,
+        };
+        setTaskText("");
+
+        return [...state, newTask];
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+      default:
+        return state;
+    }
+  };
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatchTasks({ type: "ADD" });
+  };
+  const removeTask = (id) => {
+    dispatchTasks({ type: "DELETE", id: id });
+  };
+
   return (
     <div>
       <h2>Hook useReducer</h2>
       <p>Número: {number}</p>
       <button onClick={dispatch}>Alterar o número!</button>
+      <h3>Tarefas:</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)}>
+          {task.text}
+        </li>
+      ))}
       <hr />
     </div>
   );
