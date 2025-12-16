4a6250b10959282e3632cb012a8b53c5497b4073
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-12-16 08:22:44 -0300

    feat: demonstrate useRef to track component renders

--- react-hooks/src/components/HookUseRef.jsx
+++ react-hooks/src/components/HookUseRef.jsx
@@ -2,11 +2,21 @@ import { useEffect, useState, useRef } from "react";
 
 const HookUseRef = () => {
   const numberRef = useRef(0);
-  const [number, setNumber] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [counterB, setCounterB] = useState(0);
+
+  useEffect(() => {
+    numberRef.current = numberRef.current + 1;
+  });
 
   return (
     <div>
       <h2>UseRef</h2>
+      <p>O componente renderizou: {numberRef.current} vezes.</p>
+      <p>Counter 1: {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Contador A</button>
+      <p>Counter 2: {counterB}</p>
+      <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
       <hr />
     </div>
   );
