f557c8e521bf4547f84e5d3c33bf35bc1bc0843c
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-12-16 10:34:46 -0300

    feat: add counter and function prop to useCallback example

--- react-hooks/src/components/HookUseCallback.jsx
+++ react-hooks/src/components/HookUseCallback.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { useCallback, useState } from "react";
+
+import List from "./List";
 
 const HookUseCallback = () => {
+  const [counter, setCounter] = useState(0);
+
+  const getItemsFromDataBase = () => {
+    return [1, 2, 3];
+  };
+
   return (
     <div>
       <h2>UseCallback</h2>
+      <List getItems={getItemsFromDataBase}></List>
+      <button onClick={() => setCounter(counter + 1)}>Alterar</button>
+      <p>{counter}</p>
       <hr />
     </div>
-  )
-}
+  );
+};
 
-export default HookUseCallback
+export default HookUseCallback;
---
+++ react-hooks/src/components/List.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const List = ({ getItems }) => {
+  return <div>List</div>;
+};
+
+export default List;
