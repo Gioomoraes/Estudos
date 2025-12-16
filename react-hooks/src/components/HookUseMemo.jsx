dc76195f9e9439d3281cc196a465d3d6ea6dbf1f
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-12-16 11:46:47 -0300

    perf: memoize premium numbers to avoid unnecessary re-renders

--- react-hooks/src/components/HookUseMemo.jsx
+++ react-hooks/src/components/HookUseMemo.jsx
@@ -1,9 +1,21 @@
 import { useMemo, useState, useEffect } from "react";
 
 const HookUseMemo = () => {
+  const [number, setNumber] = useState(0);
+
+  const premiumNumbers = useMemo(() => {
+    return ["0", "100", "200", "300"];
+  }, []);
+
+  useEffect(() => {
+    console.log("premium numbers foi alterado");
+  }, [premiumNumbers]);
+
   return (
     <div>
       <h2>UseMemo</h2>
+      <input type="text" onChange={(e) => setNumber(e.target.value)} />
+      {premiumNumbers.includes(number) ? <p>Acertou o número</p> : ""}
       <hr />
     </div>
   );
