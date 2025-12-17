d23ae60412ed35609a0fc18b32ee961b2532eeed
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-12-17 08:33:39 -0300

    feat: add useEffect and useLayoutEffect execution order example

--- react-hooks/src/components/HookUseLayoutEffect.jsx
+++ react-hooks/src/components/HookUseLayoutEffect.jsx
@@ -1,9 +1,24 @@
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useState, useEffect } from "react";
 
 const HookUseLayoutEffect = () => {
+  const [name, setName] = useState("NOME1");
+
+  useEffect(() => {
+    console.log("2");
+    setName("NOME2");
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("1");
+    setName("NOME3");
+  }, []);
+
+  console.log(name);
+
   return (
     <div>
       <h2>UseLayoutEffect</h2>
+      <p>Nome:{name}</p>
       <hr />
     </div>
   );
